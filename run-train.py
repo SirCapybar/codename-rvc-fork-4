f14343c27d7cf2f9ef687458fecc9d0724b149e8
Doctor-Ned <apodwysocki@gmail.com>
2026-03-16 12:46:48 +0100

    Add run-train.py

---
+++ run-train.py
@@ -0,0 +1,196 @@
+import argparse
+import logging
+import os
+import subprocess
+import shutil
+import sys
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("-n", "--name", help="Model name", required=True)
+    parser.add_argument("-g", "--gpu", help="GPU device", default="0")
+    parser.add_argument(
+        "-d",
+        "--dataset",
+        help="Dataset path",
+    )
+    parser.add_argument(
+        "-s",
+        "--sample-rate",
+        type=int,
+        choices=[24000, 32000, 40000, 48000],
+        help="Sample rate",
+        required=True,
+    )
+    parser.add_argument("-e", "--epochs", type=int, help="Epoch count")
+    parser.add_argument("-b", "--batch-size", type=int, help="Batch size")
+    parser.add_argument(
+        "-f",
+        "--epoch-save-frequency",
+        type=int,
+        help="Epoch save frequency",
+        default=10,
+    )
+    parser.add_argument("-G", "--pretrain-g", help="Pretrain G model link")
+    parser.add_argument("-D", "--pretrain-d", help="Pretrain D model link")
+    parser.add_argument(
+        "-r", "--restore-dir", help="If provided, this directory will be restored first"
+    )
+    parser.add_argument(
+        "-n",
+        "--normalization-mode",
+        choices=["none", "post_peak", "post_peak_rvc", "post_rms"],
+        help="Normalization mode",
+        default="post_rms",
+    )
+    parser.add_argument(
+        "--nh",
+        "--no-high-pass",
+        dest="high_pass",
+        action="store_false",
+        help="Disable high-pass filter",
+    )
+    parser.add_argument(
+        "--np",
+        "--no-preprocess",
+        dest="preprocess",
+        action="store_false",
+        help="Disable preprocess step",
+    )
+    parser.add_argument(
+        "--ne",
+        "--no-extract",
+        dest="extract",
+        action="store_false",
+        help="Disable extract step",
+    )
+    parser.add_argument(
+        "--ni",
+        "--no-index",
+        dest="index",
+        action="store_false",
+        help="Disable index step",
+    )
+    parser.add_argument(
+        "--nt",
+        "--no-train",
+        dest="train",
+        action="store_false",
+        help="Disable train step",
+    )
+    args = parser.parse_args()
+    if args.dataset and not os.path.isdir(args.dataset):
+        parser.error(f"Dataset path '{args.dataset}' doesn't exist")
+    if args.preprocess or args.extract or args.index:
+        if not args.dataset:
+            parser.error(
+                "-d/--dataset must be provided for preprocess/extract/index step"
+            )
+    if args.pretrain_g or args.pretrain_d:
+        if not args.pretrain_g or not args.pretrain_d:
+            parser.error(
+                "If a custom pretrain is used, both G and D links should be provided"
+            )
+    if args.train:
+        if not args.epochs:
+            parser.error("-e/--epochs must be provided for training")
+        if not args.batch_size:
+            parser.error("-b/--batch-size must be provided for training")
+        if not args.epoch_save_frequency:
+            parser.error("-f/--epoch-save-frequency must be provided for training")
+    if args.restore_dir and not os.path.isdir(args.restore_dir):
+        parser.error(f"Restore directory '{args.restore_dir}' doesn't exist")
+    return args
+
+
+def main():
+    args = parse_args()
+    logging.basicConfig(
+        format="[%(asctime)s][%(levelname)s][%(funcName)s:%(lineno)d] %(message)s",
+        level=logging.DEBUG,
+        datefmt="%H:%M:%S",
+    )
+    model = (
+        f"{args.name}_{args.sample_rate / 1000}k"
+        + (f"_{args.normalization_mode}" if args.normalization_mode != "none" else "")
+        + ("_nhp" if not args.high_pass else "")
+    )
+    logs_path = os.path.realpath(os.path.join(os.path.dirname(__file__), "logs", model))
+    logging.info(f"Output path: {logs_path}")
+    if args.restore_dir:
+        if os.path.isdir(logs_path):
+            shutil.rmtree(logs_path)
+        logging.info(f"Restoring directory '{args.restore_dir}'...")
+        shutil.copytree(args.restore_dir, logs_path)
+    CMD_BASE = ["uv", "run", "core.py"]
+    if args.preprocess:
+        cmd_preprocess = CMD_BASE + [
+            "preprocess",
+            "--model_name",
+            model,
+            "--dataset_path",
+            args.dataset,
+            "--sample_rate",
+            args.sample_rate,
+        ]
+        logging.info("Preprocessing...")
+        p = subprocess.run(cmd_preprocess)
+        p.check_returncode()
+    if args.extract:
+        cmd_extract = CMD_BASE + [
+            "extract",
+            "--model_name",
+            model,
+            "--sample_rate",
+            args.sample_rate,
+            "--gpu",
+            args.gpu,
+        ]
+        logging.info("Extracting...")
+        p = subprocess.run(cmd_extract)
+        p.check_returncode()
+    if args.index:
+        cmd_index = CMD_BASE + ["index", "--model_name"]
+        logging.info("Generating index...")
+        p = subprocess.run(cmd_index)
+        p.check_returncode()
+    if args.train:
+        cmd_train = CMD_BASE + [
+            "train",
+            "--model_name",
+            model,
+            "--epoch_save_frequency",
+            args.epoch_save_frequency,
+            "--total_epoch_count",
+            args.epochs,
+            "--sample_rate",
+            args.sample_rate,
+            "--save_only_latest_net_models=True",
+            "--batch_size",
+            args.batch_size,
+        ]
+        if args.pretrain_g:
+            G_PATH = "pretrain_G.pth"
+            D_PATH = "pretrain_D.pth"
+            cmd_train += [
+                "--custom_pretrained=True",
+                "--g_pretrained_path",
+                G_PATH,
+                "--d_pretrained_path",
+                D_PATH,
+            ]
+            logging.info("Downloading custom generator...")
+            p = subprocess.run("wget", "-O", G_PATH, args.pretrain_g)
+            p.check_returncode()
+            logging.info("Downloading custom discriminator...")
+            p = subprocess.run("wget", "-O", D_PATH, args.pretrain_d)
+            p.check_returncode()
+        logging.info("Training...")
+        p = subprocess.run(cmd_train)
+        p.check_returncode()
+    logging.info("Done!")
+
+
+if __name__ == "__main__":
+    main()
